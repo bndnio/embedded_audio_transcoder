cad1bd9ae5193a53ce803d77e8b2a303924efa99
Brendon Earl <bearl@uvic.ca>
2019-07-15 13:23:47 -0700

    Add encode steps 1, 3, 4, 5

--- src/encode.c
+++ src/encode.c
@@ -4,26 +4,34 @@
 // Step 1
 SignMag conv_sign_mag(int to_convert)
 {
+    SignMag sign_mag;
+    sign_mag.sign = to_convert >= 0 ? POS : NEG;
+    sign_mag.mag = to_convert >= 0 ? to_convert : -to_convert;
+
+    return sign_mag;
 }
 
 // Step 2
-int calc_chord(int magnitude)
+char calc_chord(unsigned int magnitude)
 {
 }
 
 // Step 3
-int extract_steps(int chord)
+char extract_steps(char chord)
 {
+    return 0x0F & chord;
 }
 
 // Step 4
-int assemble_codeword(Sign sign, int chord, int step)
+char assemble_codeword(Sign sign, char chord, char step)
 {
+    return (char)sign << 7 | chord << 4 | step;
 }
 
 // Step 5
-int invert_codeword(int codeword)
+int invert_codeword(char codeword)
 {
+    return !codeword;
 }
 
 /**
