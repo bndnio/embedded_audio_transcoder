bd5f988b296bf229d29c6c3a5dcd34dc23fa95fc
cmaricle <courtneymaricle@gmail.com>
2019-08-03 17:34:12 -0700

    add missing function tests to test encode - will fix other broken tests in another commit

--- test/main.c
+++ test/main.c
@@ -9,6 +9,8 @@ int main()
     // test_decode();
     test_conv_sign_mag();
     test_calc_chord();
+    test_extract_steps();
+    test_assemble_codeword();
     check_summary();
     return 0;
 }
--- test/test_encode.c
+++ test/test_encode.c
@@ -159,8 +159,148 @@ void test_calc_chord()
 void test_extract_steps()
 {
     check_equal(
+        "check extract_steps with chord 0",
+        0x03,
+        extract_steps(0x009F, calc_chord(0x009F)),
+        puts_b16);
+
+    check_equal(
         "check extract_steps with chord 1",
-        0b0,
-        calc_chord(0x100F),
+        0x0F,
+        extract_steps(0x01FF, calc_chord(0x01FF)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 2",
+        0x09,
+        extract_steps(0x0320, calc_chord(0x0320)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 3",
+        0x04,
+        extract_steps(0x050F, calc_chord(0x050F)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 4",
+        0x06,
+        extract_steps(0x0B00, calc_chord(0x0B00)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 5",
+        0x03,
+        extract_steps(0x1300, calc_chord(0x1300)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 6",
+        0x07,
+        extract_steps(0x2F00, calc_chord(0x2F00)),
+        puts_b16);
+
+    check_equal(
+        "check extract_steps with chord 7",
+        0x03,
+        extract_steps(0x4F00, calc_chord(0x4F00)),
+        puts_b16);
+}
+
+void test_assemble_codeword() {
+    check_equal(
+        "test assemble codeword with 0 chord and negative sign",
+        0x8F,
+        assemble_codeword(0, 0x0, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 0 chord and pos sign",
+        0x0F,
+        assemble_codeword(1, 0x0, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 1 chord and negative sign",
+        0x9F,
+        assemble_codeword(0, 0x01, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 1 chord and pos sign",
+        0x1F,
+        assemble_codeword(1, 0x01, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 2 chord and negative sign",
+        0xAF,
+        assemble_codeword(0, 0x02, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 2 chord and pos sign",
+        0x2F,
+        assemble_codeword(1, 0x02, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 3 chord and negative sign",
+        0xBF,
+        assemble_codeword(0, 0x03, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 3 chord and pos sign",
+        0x3F,
+        assemble_codeword(1, 0x03, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 4 chord and negative sign",
+        0xCF,
+        assemble_codeword(0, 0x04, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 4 chord and pos sign",
+        0x4F,
+        assemble_codeword(1, 0x04, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 5 chord and negative sign",
+        0xDF,
+        assemble_codeword(0, 0x05, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 5 chord and pos sign",
+        0x5F,
+        assemble_codeword(1, 0x05, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 6 chord and negative sign",
+        0xEF,
+        assemble_codeword(0, 0x06, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 6 chord and pos sign",
+        0x6F,
+        assemble_codeword(1, 0x06, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 7 chord and negative sign",
+        0xFF,
+        assemble_codeword(0, 0x07, 0xF),
+        puts_b16);
+
+    check_equal(
+        "test assemble codeword with 7 chord and pos sign",
+        0x7F,
+        assemble_codeword(1, 0x07, 0xF),
         puts_b16);
 }
