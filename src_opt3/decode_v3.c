ba2a5c8ab166bf09b43f9f6b7f2628e25261be0f
Brendon Earl <bearl@uvic.ca>
2019-08-05 14:59:39 -0700

    Fix decode behaviour with missing bit mask

--- src_opt3/decode_v3.c
+++ src_opt3/decode_v3.c
@@ -9,7 +9,7 @@ uint16_t decode_v3(register char input)
 {
     input = ~input;
 
-    register uint16_t chord = (input >> 4) + 3;
+    register uint16_t chord = ((0x70 & input) >> 4) + 3;
 
     register int16_t decoded = ((0x10 | (0x0F & input)) << chord) - 132;
 
